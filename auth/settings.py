4bb0b9613d1bf7db4fa3186234beefccc6a3e95a
Ankush <ankushbisht72@gmail.com>
2023-06-12 17:46:11 +0530

    check

--- auth/settings.py
+++ auth/settings.py
@@ -138,8 +138,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES':(
         'rest_framework.permissions.IsAdminUser',
