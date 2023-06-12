c261a50ba91eeede91ab43afff8c3546064f5a88
Ankush <ankushbisht72@gmail.com>
2023-06-12 14:39:27 +0530

    Fixed the bug

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.premissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
 import jwt , datetime
 
@@ -120,5 +120,4 @@ class RatingView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    
     
