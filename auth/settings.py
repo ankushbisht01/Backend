1c318c3f23731ff0d05b11502e29723b083f5813
Ankush <ankushbisht72@gmail.com>
2023-06-13 13:41:47 +0530

    back to old CROS

--- auth/settings.py
+++ auth/settings.py
@@ -166,6 +166,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "user.User"
 
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     
     "https://backend-production-9ac3.up.railway.app",
