da2574a3950b6fe13651aa2037b76ec5039097a4
Ankush <ankushbisht72@gmail.com>
2023-06-14 09:50:22 +0530

    changed to python 10x

--- auth/settings.py
+++ auth/settings.py
@@ -177,7 +177,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     
     "https://backend-production-9ac3.up.railway.app",
-    "http://localhost:3000/",
+    "http://localhost:3000",
     
 ]
 
